6b18cc03a8228a7c7504da8cb8ce9ca40ff249bc
Nessit2610 <letien26102003@gmail.com>
2024-08-02 19:10:57 +0700

    Start Project

---
+++ src/main/java/com/mbcservice/MBCService/MbcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mbcservice.MBCService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbcServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbcServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbcservice/MBCService/MbcServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbcservice.MBCService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbcServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
